21cb31ce29fc465d17c672f04b82a8d3f7465d20
user <viktorija.99@mail.ru>
2018-10-10 09:54:16 +0300

    20181010_09_50

---
+++ test_20181010_1.py
@@ -0,0 +1,161 @@
+x = 5
+if x == 5:
+    print('Equals 5')
+if x > 4:
+    print('Ģreater than 4')
+if x >= 5:
+    print('Greater than 5')
+if x < 6: print('Less than 6')
+if x <= 5:
+    print('Equals 5')
+    if x !=6:
+        print('Not equal 6')
+x = 5
+print('Before 5')
+if x == 5:
+    print('ls 5')
+    print('Is Still 5')
+    print('Third 5')
+print('Afterwards 5')
+print('Before 6')
+if x== 6:
+    print('Is 6')
+    print('Is Still 6')
+    print('Third 6')
+print('Āfterwards 6')
+print('Afterwards 6')
+
+x = 5
+if x > 2:
+    print('Bigger than 2')
+    print('Still bigger')
+print('Done with 2')
+for i in range(5):
+    print(i)
+    if i > 2:
+        print('Bigger than 2')
+        print('Done with i', i)
+        print('All Done')
+print('Ar šo mēs varam risināt (i) vērtību eletroniski')
+x = 42
+if x > 1:
+    print('More than one')
+    if x < 100:
+        print('Less than 100')
+        print('All done')
+print('Ievāditie numuru skaits, kur var izvēlēt ciparus (ye or no)')
+x = 4
+if x > 2:
+    print('Bigger')
+else:
+    print('Smaller')
+    print('All done')
+
+x = 0
+if x < 2:
+    print('small')
+elif x < 10:
+        print('Medium')
+else:
+    print('LARGE')
+print('All done')
+
+x = 5
+if x < 2:
+    print('small')
+elif x < 10:
+        print('Medium')
+else:
+    print('LARGE')
+print('All done')
+
+x = 20
+if x < 2:
+    print('small')
+elif x < 10:
+        print('Medium')
+else:
+    print('LARGE')
+print('All done')
+# No Else
+x = 5
+if x < 2:
+    print('Small')
+elif x < 10:
+    print('Medium')
+print('All done')
+
+if x < 2 :
+ print('Small')
+elif x < 10:
+  print('Medium')
+elif x < 20:
+    print('Big')
+elif x < 40:
+    print('Large')
+elif x < 100:
+    print('Huge')
+else:
+  print('Ginormous')
+
+if x < 2:
+ print('Below 2')
+elif x >= 2: 
+ print('Two or more')
+else :
+ print('Something else')
+
+if x < 2:
+ print('Below 2')
+elif x < 20 :
+ print('Below 20')
+elif x < 10 : 
+ print('Below 10')
+else :
+ print('Something  else')
+
+astr = 'Hello Bob'
+try:
+   istr= int(astr)
+except:
+  istr= -1
+
+print('First', istr)
+
+astr= '123'
+try:
+    istr= int(astr)
+except:
+      istr= -1
+
+print('Second', istr)
+
+astr = 'Bob'
+try:
+ print('Hello') 
+ istr= int(astr)
+ print('There')
+except:
+   istr = -1
+print('Done', istr) 
+
+
+rawstr= input('Enter a number:')
+try:
+  ival= int(rawstr)
+except:
+  ival= -1
+
+if ival > 0:
+    print('Nice work')
+else:
+   print('Not a number')
+
+   
+            
+        
+
+    
+
+
+
