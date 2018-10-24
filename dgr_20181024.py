aeea470b6fef45f4cf893a47a9e6b70b59645c2b
user <viktorija.99@mail.ru>
2018-10-24 09:53:39 +0300

    2018201824_9_55_

---
+++ dgr_20181024.py
@@ -0,0 +1,28 @@
+while True:
+    line = input('>')
+    if line == 'done':
+        break
+    print(line)
+    print('Done!') 
+while True:
+ line = input('>')
+if line =='done':
+ print(line)
+print('Done!')
+
+
+for i in [5, 4, 3, 2, 1]:
+ print(i)
+print('Blastoff!')
+
+friends = ['Joseph', 'Glenn', 'Sally']
+for friend in friends: 
+ print('Happy New Year:',friend)
+print('Done!')
+
+print('Before')
+for thing in[9, 41, 12, 3, 74, 15]:
+ print(thing)
+print('After')
+
+
