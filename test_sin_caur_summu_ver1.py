f1ec6de0e74a6672e5cd6bacc06583d27dd20589
user <viktorija.99@mail.ru>
2018-10-31 11:16:25 +0200

    sin_caur_summu_ver1

---
+++ test_sin_caur_summu_ver1.py
@@ -0,0 +1,32 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver1.py =============
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) = 0.84
+a0 = 1.00 S0 = 1.00
+a1 = -0.17 S1 = 0.83
+a2 = 0.01 S2 = 0.84
+a3 = -0.00 S3 = 0.84
+>>> 1
+1
+>>> 1.5
+1.5
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver1.py =============
+Lietotāj, lūdzu, ievadi argumentu (x): 1.5
+sin(1.50) = 1.00
+a0 = 1.50 S0 = 1.50
+a1 = -0.56 S1 = 0.94
+a2 = 0.06 S2 = 1.00
+a3 = -0.00 S3 = 1.00
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver1.py =============
+Lietotāj, lūdzu, ievadi argumentu (x): 2.89
+sin(2.89) = 0.25
+a0 = 2.89 S0 = 2.89
+a1 = -4.02 S1 = -1.13
+a2 = 1.68 S2 = 0.55
+a3 = -0.33 S3 = 0.21
+>>> 
