421b326710bf7d3d76c8597f074ae0d906a22a27
user <viktorija.99@mail.ru>
2018-10-31 12:02:27 +0200

    sin_caur_summu_ver3

---
+++ test_sin_caur_summu_ver3.py
@@ -0,0 +1,58 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzū, ievadi argument (x): 1.5
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver3.py", line 7, in <module>
+    prin("sin(%.2f) = %6.2f"%(x,y))
+NameError: name 'prin' is not defined
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzū, ievadi argument (x): 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzū, ievadi argument (x): 1.5
+sin(1.50) =   1.00
+a0 =   1.50 S0 =   1.50
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver3.py", line 20, in <module>
+    print("a1 = %6.2f S1 = %6.2"%(a,S))
+ValueError: incomplete format
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzū, ievadi argument (x): 1.5
+sin(1.50) =   1.00
+a0 =   1.50 S0 =   1.50
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver3.py", line 20, in <module>
+    print("a1 = %6.2f S1 = %6.2"%(a,S))
+ValueError: incomplete format
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzū, ievadi argument (x): 1.5
+sin(1.50) =   1.00
+a0 =   1.50 S0 =   1.50
+a1 =  -0.56 S1 =   0.94
+a2 =   0.06 S2 =   1.00
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver3.py", line 34, in <module>
+    print("a3 = %6.2f S3 = %6.2"%(a,S))
+ValueError: incomplete format
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzū, ievadi argument (x): 1.5
+sin(1.50) =   1.00
+a0 =   1.50 S0 =   1.50
+a1 =  -0.56 S1 =   0.94
+a2 =   0.06 S2 =   1.00
+a3 =  -0.00 S3 =   1.00
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver3.py =============
+Lietotāj, lūdzū, ievadi argument (x): 2.89
+sin(2.89) =   0.25
+a0 =   2.89 S0 =   2.89
+a1 =  -4.02 S1 =  -1.13
+a2 =   1.68 S2 =   0.55
+a3 =  -0.33 S3 =   0.21
+>>> 
