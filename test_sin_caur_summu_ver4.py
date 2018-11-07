cecf17d80fa508a0ccb11c751a0a8f5dbf8c9023
user <viktorija.99@mail.ru>
2018-11-07 08:45:43 +0200

    sin_caur_summu_ver4.py

---
+++ test_sin_caur_summu_ver4.py
@@ -0,0 +1,67 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver4.py =============
+Lietitāj, lūdzu, ievadi argumentu (x): 1.09
+sin(1.09) =   0.89
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver4.py", line 11, in <module>
+    print("a0 = %6.2f S0 = %6.2"%(a,S))
+ValueError: incomplete format
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver4.py =============
+Lietitāj, lūdzu, ievadi argumentu (x): 1.09
+sin(1.09) =   0.89
+a0 =   1.09 S0 =   1.09
+a1 =  -7.77 S1 =  -6.68
+a2 =   0.46 S2 =  -6.22
+a3 =  -0.01 S3 =  -6.23
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver4.py =============
+Lietitāj, lūdzu, ievadi argumentu (x): 2.89
+sin(2.89) =   0.25
+a0 =   2.89 S0 =   2.89
+a1 = -144.83 S1 = -141.94
+a2 =  60.48 S2 = -81.46
+a3 = -12.03 S3 = -93.48
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver4.py =============
+Lietitāj, lūdzu, ievadi argumentu (x): 0.95
+sin(0.95) =   0.81
+a0 =   0.95 S0 =   0.95
+a1 =  -5.14 S1 =  -4.19
+a2 =   0.23 S2 =  -3.96
+a3 =  -0.00 S3 =  -3.97
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver4.py =============
+Lietitāj, lūdzu, ievadi argumentu (x): 1.5
+sin(1.50) =   1.00
+a0 =   1.50 S0 =   1.50
+a1 = -20.25 S1 = -18.75
+a2 =   2.28 S2 = -16.47
+a3 =  -0.12 S3 = -16.59
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver4.py =============
+Lietitāj, lūdzu, ievadi argumentu (x): 
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver4.py", line 3, in <module>
+    x = float(input("Lietitāj, lūdzu, ievadi argumentu (x): "))
+ValueError: could not convert string to float: 
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver4.py =============
+Lietitāj, lūdzu, ievadi argumentu (x): 
+Lietitāj, lūdzu, ievadi argumentu (x):
+Traceback (most recent call last):
+  File "/home/user/RTR105/sin_caur_summu_ver4.py", line 3, in <module>
+    x = float(input("Lietitāj, lūdzu, ievadi argumentu (x): "))
+ValueError: could not convert string to float: 'Lietitāj, lūdzu, ievadi argumentu (x):'
+>>> 
+============= RESTART: /home/user/RTR105/sin_caur_summu_ver4.py =============
+Lietotāj, lūdzu, ievadi argumentu (x): 1
+sin(1.00) =   0.84
+a0 =   1.00 S0 =   1.00
+a1 =  -6.00 S1 =  -5.00
+a2 =   0.30 S2 =  -4.70
+a3 =  -0.01 S3 =  -4.71
+>>> 
