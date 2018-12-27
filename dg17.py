753f07163e7ef9e8448f8c7e4a845860bbdad263
viktorija.99 <viktorija.99@mail.ru>
2018-12-27 23:27:15 +0200

    170.py

---
+++ dg17.py
@@ -0,0 +1,8 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: /home/viktorija99/RTR105/170.py ==================
+Dotajaa intervaalaa sakne(s) ir!
+Sakne ir:  3.1425781250000004
+>>> 
